950270359218e494bcd60cdafc92e0d47156b416
Akantonio <adrianvantonio@yahoo.com>
2021-03-31 09:30:05 -0800

    added set functions and virtual functions for box class

--- box.h
+++ box.h
@@ -5,12 +5,17 @@
 #ifndef HOMEWORK4_BOX_H
 #define HOMEWORK4_BOX_H
 
-
+#include <string>
+#include <iostream>
 class box {
 public:
     box(int x,int y);
     int getWidth() const;
     int getHeight()const;
+    void setWidth();
+    void setHeight();
+    virtual void print(std::ostream&os)=0;
+    virtual std::string type()=0;
 private:
     int _width;
     int _height;
